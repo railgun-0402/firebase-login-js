74c35cdf126bbd17f5fe210e548593f39f3629e5
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-11-09 01:25:47 +0900

    spinner

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,49 @@
 import { signInWithPopup } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import { auth, provider } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Spinner from "react-activity/dist/Spinner";
+import "react-activity/dist/Spinner.css";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signInWithGoogle = () => {
+    setIsLoading(true);
+    // FSを使用してログイン
+    signInWithPopup(auth, provider)
+      .then(() => {
+        // サインインに成功した場合、インジケータ削除
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // サインインに失敗した場合も、インジケータ削除
+        setIsLoading(false);
+        console.error(error);
+      });
+  };
+
   // どのアカウントで認証してるか、状態を確認
   // nullなら未登録
   const [user] = useAuthState(auth);
   return (
     <div>
-      {user ? (
+      {isLoading ? (
+        // インジケータ表示
+        <>
+          <div className="spinner-container">
+            <Spinner />
+          </div>
+          <div>Loading...</div>
+        </>
+      ) : // インジケータ非表示
+      user ? (
         <>
           <UserInfo />
           <SignOutButton />
         </>
       ) : (
-        <SignInButton />
+        <SignInButton signInWithGoogle={signInWithGoogle} />
       )}
     </div>
   );
@@ -24,12 +52,7 @@ const Home = () => {
 export default Home;
 
 // サインイン
-function SignInButton() {
-  const signInWithGoogle = () => {
-    // FSを使用してログイン
-    signInWithPopup(auth, provider);
-  };
-
+function SignInButton({ signInWithGoogle }) {
   return (
     <button onClick={signInWithGoogle}>
       <p>Googleでサインイン</p>
